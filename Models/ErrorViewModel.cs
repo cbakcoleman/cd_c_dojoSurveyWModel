84f3e91676f790ac0dfa4fd456b65c19537de6b9
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-05 13:19:27 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cd_c_dojoSurveyWModel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
